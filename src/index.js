0d34ced58edf7538e7879320570728dd530eb731
Nikolay Vinogradov <was.bic@gmail.com>
2022-07-09 22:23:44 +0300

    add calc.js Выполнено задание. Урок 24

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal';
 import form from './modules/form';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import calc from './modules/calc';
 
 timer('10 jule 2022');
 menuburg();
@@ -11,3 +12,4 @@ modal();
 form();
 tabs();
 slider();
+calc(100);
